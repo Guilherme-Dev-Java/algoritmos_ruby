9fcadb81a1d8f6a504759d53d7988e831779d66b
Carlos Guilherme <guilhermeagripino@gmail.com>
2024-05-08 10:41:11 -0300

    fix: correção na criação do array ordenado e na busca pelo indice

--- dois_numeros.rb
+++ dois_numeros.rb
@@ -1,16 +1,17 @@
 def dois_numeros (nums, target)
     
-    nums_ordenado = nums.sort
+    nums_ordenado = nums.dup.sort 
     inicio = 0
-    fim = nums_ordenado.length-1
+    fim = nums_ordenado.length - 1
+
     
-    while (inicio < fim)
+    while inicio < fim
           
         soma = nums_ordenado[inicio] + nums_ordenado[fim]
 
         if soma == target
             indice_1 = nums.index(nums_ordenado[inicio])
-            indece_2 = nums.index (nums_ordenado[fim])
+            indece_2 = nums.rindex (nums_ordenado[fim])
 
             return [indice_1, indece_2]
 
@@ -30,7 +31,7 @@ end
 
 
 
-nums = [0,1,2,2,3,0,4,2]
-val = 7
+nums = [3,3]
+val = 6
 
 p dois_numeros(nums, val)
